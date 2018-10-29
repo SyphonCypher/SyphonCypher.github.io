787f0bf0bea4fb7168cdcea2686702351f057e97
SyphonCypher <tim_horta@outlook.com>
2018-10-29 18:23:32 -0400

    added javascript functionality

---
+++ main.js
@@ -0,0 +1,136 @@
+(function() {
+
+    setUpPage();
+  
+    function setUpPage() {
+      addNavEventListeners();
+      addArticleEventListeners();
+      addWindowEventListeners();
+      addModalEventListeners();
+      addSliderEventListeners();
+    }
+
+    function addNavEventListeners() {
+  
+      const nav = document.querySelector('nav');
+  
+      const navClickHandler = function() {
+        this.classList.toggle('open');
+        event.stopPropagation();
+      };
+      console.log (nav);
+      nav.addEventListener('click', navClickHandler);
+  
+      const bodyClickHandler = function() {
+        nav.classList.remove('open');
+      };
+  
+      document.body.addEventListener('click', bodyClickHandler);
+    }
+
+    function addArticleEventListeners() {
+  
+      const articles = document.querySelectorAll('#myProjects article');
+  
+      for (let article of articles) {
+        let button = article.querySelector('button');
+  
+        button.addEventListener('click', function() {
+          article.classList.toggle('expanded');
+          button.innerHTML = article.classList.contains('expanded') ?
+            'Hide Details' : 'Show Details';
+        });
+      }
+    }
+
+    function addWindowEventListeners() {
+  
+      const windowResizeHandler = function() {
+  
+        const nav = document.querySelector('nav');
+        nav.classList.remove('open');
+  
+        const articles = document.querySelectorAll('#myProjects article');
+        
+        for (let article of articles) {
+          article.classList.remove('expanded');
+        }
+      };
+  
+      window.addEventListener('resize', windowResizeHandler); 
+    }
+  
+    function addModalEventListeners() {
+  
+      const modalTriggerElement = document.querySelector('#modal-trigger');
+  
+      const modalTriggerClickHandler = function() {
+        document.body.classList.add('modal-open');
+      };
+  
+      modalTriggerElement.addEventListener('click', modalTriggerClickHandler);
+  
+      const modalCloseButton = document.querySelector('#modal button');
+  
+      const modalCloseButtonClickHandler = function() {
+        document.body.classList.remove('modal-open');
+      };
+  
+      modalCloseButton.addEventListener('click', modalCloseButtonClickHandler);
+    }
+
+    function addSliderEventListeners() {
+  
+      const sliderPrev = document.getElementById('slider-prev');
+      const sliderNext = document.getElementById('slider-next');
+      const sliderFrame = document.getElementById('slider-frame');
+      const sliderCaption = document.getElementById('slider-caption');
+  
+      const slides =
+        Array.from(sliderFrame.querySelectorAll('div[data-src]'))
+          .map(div => {
+            return {
+              imageUrl: div.getAttribute('data-src'),
+              caption: div.textContent.trim(),
+            };
+          });
+  
+      let sliderIndex = 0;
+      displaySliderImage();
+  
+      function displaySliderImage() {
+        let { imageUrl, caption } = slides[sliderIndex];
+        sliderFrame.style.backgroundImage = `url('${imageUrl}')`;
+        let count = `(${sliderIndex+1}/${slides.length}) `;
+        sliderCaption.innerHTML = count + caption;
+        sliderCaption.classList.add('flash');
+        setTimeout(() => {
+          sliderCaption.classList.remove('flash');
+        }, 0);
+      }
+  
+      function displayPrevSliderImage() {
+        sliderIndex--;
+        if (sliderIndex < 0) {
+          sliderIndex = slides.length - 1;
+        }
+        displaySliderImage();
+      }
+  
+      function displayNextSliderImage() {
+        sliderIndex++;
+        if (sliderIndex === slides.length) {
+          sliderIndex = 0;
+        }
+        displaySliderImage();
+      }
+  
+      sliderPrev.addEventListener('click', displayPrevSliderImage);
+      sliderNext.addEventListener('click', displayNextSliderImage);
+  
+      setInterval(function() {
+        displayNextSliderImage();
+      }, 4000);
+    }
+  
+  })();
